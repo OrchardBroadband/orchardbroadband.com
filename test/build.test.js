696ec1432d61606aee08e6bad43a7fa134bd06cb
David Cox <losttime.shuffle@gmail.com>
2017-08-09 16:05:08 +0000

    Add tests

---
+++ test/build.test.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+describe('build home', () => {
+	test('exists', () => {
+		expect(fs.existsSync('build/index.html')).toBeTruthy()
+	});	
+});
+
+describe('build css', () => {
+	test('exists', () => {
+		expect(fs.existsSync('build/css/main.css')).toBeTruthy()
+	});	
+});
+
+describe('build platform', () => {
+	test('manifest exists', () => {
+		expect(fs.existsSync('build/platform/manifest.json')).toBeTruthy()
+	});	
+	
+	test('browserconfig exists', () => {
+		expect(fs.existsSync('build/platform/browserconfig.xml')).toBeTruthy()
+	});	
+});
+
+describe('build blog home', () => {
+	test('exists', () => {
+		expect(fs.existsSync('build/blog/index.html')).toBeTruthy()
+	});	
+});
+
+describe('build blog posts', () => {
+	test('exists', () => {
+		expect(fs.existsSync('build/blog/2017/08/08/first-post/index.html')).toBeTruthy()
+	});	
+});
